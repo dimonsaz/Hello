6f227ab0839673e0f29adafad1907dd13a039613
dimonsaz <37563015+dimonsaz@users.noreply.github.com>
2018-04-30 14:34:30 -0400

    .

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Docker dotNet Core 2.0 Console App!!!!! where is it?");
+        }
+    }
+}
